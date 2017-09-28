ea04d1457f9245a8d7cb7aa4b639beb181d4ca6b
Stu Heiss <sheiss@highlandsolutions.com>
2017-09-28 12:12:59 -0500

    Settings implemented

--- Main.elm
+++ Main.elm
@@ -9,12 +9,21 @@ import Html.Events exposing (onClick)
 
 
 type alias Model =
-    { notifications : Bool, autoplay : Bool, location : Bool }
+    { notifications : Bool, autoplay : Autoplay, location : Bool }
+
+
+type alias Autoplay =
+    { enabled : Bool, audio : Bool, withoutWifi : Bool }
+
+
+defaults : Model
+defaults =
+    Model False (Autoplay False False False) False
 
 
 model : Model
 model =
-    { notifications = False, autoplay = False, location = False }
+    defaults
 
 
 
@@ -32,16 +41,32 @@ init =
 
 view : Model -> Html Msg
 view model =
-    fieldset []
-        [ checkbox ToggleNotifications "Email Notifications"
-        , checkbox ToggleAutoplay "Video Autoplay"
-        , checkbox ToggleLocation "Use Location"
-        ]
+    let
+        checkboxes =
+            case model.autoplay.enabled of
+                True ->
+                    [ checkbox ToggleNotifications "Email Notifications"
+                    , checkbox ToggleAutoplay "Video Autoplay"
+                    , checkbox ToggleAutoplayAudio "Autoplay with audio"
+                    , checkbox ToggleAutoplayWithoutWifi "Autoplay without wifi"
+                    , checkbox ToggleLocation "Use Location"
+                    ]
+
+                False ->
+                    [ checkbox ToggleNotifications "Email Notifications"
+                    , checkbox ToggleAutoplay "Video Autoplay"
+                    , checkbox ToggleLocation "Use Location"
+                    ]
+    in
+        div []
+            [ fieldset [] checkboxes
+            , text (toString model)
+            ]
 
 
 checkbox : msg -> String -> Html msg
 checkbox msg name =
-    label []
+    label [ style [ ( "padding", "20px" ) ] ]
         [ input [ type_ "checkbox", onClick msg ] []
         , text name
         ]
@@ -54,14 +79,49 @@ checkbox msg name =
 type Msg
     = ToggleNotifications
     | ToggleAutoplay
+    | ToggleAutoplayAudio
+    | ToggleAutoplayWithoutWifi
     | ToggleLocation
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        _ ->
-            ( model, Cmd.none )
+        ToggleNotifications ->
+            ( { model | notifications = not model.notifications }, Cmd.none )
+
+        ToggleAutoplay ->
+            let
+                autoplay =
+                    model.autoplay
+
+                newautoplay =
+                    { autoplay | enabled = not model.autoplay.enabled }
+            in
+                ( { model | autoplay = newautoplay }, Cmd.none )
+
+        ToggleAutoplayAudio ->
+            let
+                autoplay =
+                    model.autoplay
+
+                newautoplay =
+                    { autoplay | audio = not model.autoplay.audio }
+            in
+                ( { model | autoplay = newautoplay }, Cmd.none )
+
+        ToggleAutoplayWithoutWifi ->
+            let
+                autoplay =
+                    model.autoplay
+
+                newautoplay =
+                    { autoplay | withoutWifi = not model.autoplay.withoutWifi }
+            in
+                ( { model | autoplay = newautoplay }, Cmd.none )
+
+        ToggleLocation ->
+            ( { model | location = not model.location }, Cmd.none )
 
 
 
