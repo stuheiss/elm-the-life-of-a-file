d03f457aa0d9b646e1a0641687139e131b6a58de
Stu Heiss <sheiss@highlandsolutions.com>
2017-09-30 13:58:16 -0500

    Implement List and Dict checkboxes

--- Main.elm
+++ Main.elm
@@ -4,6 +4,7 @@ import Html exposing (Html, div, span, button, text, input, label, fieldset, ul,
 import Html.Attributes exposing (type_, style, checked)
 import Html.Events exposing (onClick)
 import Set exposing (Set)
+import Dict exposing (Dict)
 
 
 -- MODEL
@@ -13,8 +14,10 @@ type alias Model =
     { notifications : Bool
     , autoplay : Autoplay
     , location : Bool
+    , animals : List ( String, Bool )
     , fruits : List String
     , selected : Set String
+    , languages : Dict String Bool
     }
 
 
@@ -32,9 +35,28 @@ listOfFruits =
     ]
 
 
+animals =
+    [ ( "cat", False )
+    , ( "dog", False )
+    , ( "horse", False )
+    ]
+
+
+languages =
+    Dict.fromList
+        [ ( "C", False )
+        , ( "perl", False )
+        , ( "swift", False )
+        , ( "python", False )
+        , ( "Elm", False )
+        , ( "php", False )
+        , ( "haskell", False )
+        ]
+
+
 defaults : Model
 defaults =
-    Model False (Autoplay False False False) False listOfFruits Set.empty
+    Model False (Autoplay False False False) False animals listOfFruits Set.empty languages
 
 
 model : Model
@@ -73,38 +95,60 @@ view model =
             List.map (\x -> div [] [ text x ]) model.fruits
     in
         div []
-            [ text "Settings:"
+            [ text "Settings (Record):"
             , fieldset [] settings
             , br [] []
-            , text "Fruits:"
-            , fieldset [] (List.map (checkboxFruits model.selected) model.fruits)
+            , text "Fruits (Set):"
+            , fieldset [] (List.map (checkboxFruit model.selected) model.fruits)
+            , br [] []
+            , text "Animals (List):"
+            , fieldset [] (List.map (\( name, isChecked ) -> checkboxAnimal ( name, isChecked )) model.animals)
+            , br [] []
+            , text "Languages (Dict):"
+            , fieldset [] (List.map (\( name, isChecked ) -> checkboxLanguage ( name, isChecked )) (Dict.toList model.languages))
             , br [] []
             , text "Model: "
             , text (toString model)
             ]
 
 
-checkboxFruits : Set String -> String -> Html Msg
-checkboxFruits selectedFruits fruit =
-    let
-        isChecked =
-            Set.member fruit selectedFruits
-
-        message =
-            if isChecked then
-                Deselect fruit
-            else
-                Select fruit
-    in
-        label [ style [ ( "display", "block" ) ] ]
-            [ input
-                [ type_ "checkbox"
-                , checked isChecked
-                , onClick message
-                ]
-                []
-            , text fruit
+checkboxLanguage : ( String, Bool ) -> Html Msg
+checkboxLanguage ( language, isChecked ) =
+    label [ style [ ( "display", "block" ) ] ]
+        [ input
+            [ type_ "checkbox"
+            , checked isChecked
+            , onClick (ToggleLanguage language)
             ]
+            []
+        , text language
+        ]
+
+
+checkboxAnimal : ( String, Bool ) -> Html Msg
+checkboxAnimal ( animal, isChecked ) =
+    label [ style [ ( "display", "block" ) ] ]
+        [ input
+            [ type_ "checkbox"
+            , checked isChecked
+            , onClick (ToggleAnimal animal)
+            ]
+            []
+        , text animal
+        ]
+
+
+checkboxFruit : Set String -> String -> Html Msg
+checkboxFruit selectedFruits fruit =
+    label [ style [ ( "display", "block" ) ] ]
+        [ input
+            [ type_ "checkbox"
+            , checked (Set.member fruit selectedFruits)
+            , onClick (ToggleFruit fruit)
+            ]
+            []
+        , text fruit
+        ]
 
 
 checkboxSetting : Bool -> String -> msg -> String -> Html msg
@@ -139,8 +183,9 @@ type Msg
     | ToggleAutoplayWithoutWifi
     | ToggleLocation
     | Noop
-    | Select String
-    | Deselect String
+    | ToggleFruit String
+    | ToggleAnimal String
+    | ToggleLanguage String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -182,14 +227,50 @@ update msg model =
         ToggleLocation ->
             ( { model | location = not model.location }, Cmd.none )
 
+        ToggleFruit fruit ->
+            let
+                newselected =
+                    if Set.member fruit model.selected then
+                        Set.remove fruit model.selected
+                    else
+                        Set.insert fruit model.selected
+            in
+                ( { model | selected = newselected }, Cmd.none )
+
+        ToggleAnimal animal ->
+            ( { model
+                | animals =
+                    List.map
+                        (\( name, checked ) ->
+                            if name == animal then
+                                ( name, not checked )
+                            else
+                                ( name, checked )
+                        )
+                        model.animals
+              }
+            , Cmd.none
+            )
+
+        ToggleLanguage language ->
+            ( { model
+                | languages =
+                    Dict.update language
+                        (\v ->
+                            case v of
+                                Nothing ->
+                                    Nothing
+
+                                Just b ->
+                                    Just (not b)
+                        )
+                        model.languages
+              }
+            , Cmd.none
+            )
+
         Noop ->
             ( model, Cmd.none )
-
-        Select fruit ->
-            ( { model | selected = Set.insert fruit model.selected }, Cmd.none )
-
-        Deselect fruit ->
-            ( { model | selected = Set.remove fruit model.selected }, Cmd.none )
 
 
 
--- README.md
+++ README.md
@@ -5,15 +5,40 @@ From a [presentation](https://www.youtube.com/watch?v=XpDsk374LDE) at Elm Europe
 
 ## Two types of checkboxes:
 
-### Settings:
+Implement solutions by chosing a good modeling strategy and let the model guide the implementation.
+
+### Part 1 - Settings (Record):
+
        Email Notifications
        Video Autoplay
        Use Location
 
-### Fruits:
-       Apple
-       Apricot
-       Banana
-       Mango
-       Orage
-       Plum
+Feture request - Add options to Video Autoplay
+
+           Autoplay with auido
+           Autoplay without wifi
+
+Implement the options in such a way that their checkboxes are only visible if Video Autoplay is checked. Maintain the options' states so that any that are checked and become hidded remain checked when they become visible again
+
+
+### Part 2 - Fruits (Set), Animals (List), Languages (Dict):
+    Fruits (Set):
+        Apple
+        Apricot
+        Banana
+        Mango
+        Orage
+        Plum
+    Animals (List):
+        Cat
+        Dog
+        Horse
+    Languages (Dict):
+        C
+        elm
+        haskell
+        perl
+        php
+        python
+        swift
+
